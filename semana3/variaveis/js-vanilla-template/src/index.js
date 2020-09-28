e5e5e171283c3ac997c359ba918e4a8cdc2ea187
Alexandre <alexandre.yukon@gmail.com>
2020-09-28 16:26:02 -0300

    produzindo exercicios aula javascript

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,101 @@
+/*
+a = 10
+b = 10
+
+console.log (b)
+
+b = 5
+console.log (a, b)
+*/
+
+/* 
+no primeiro console.log ele vai imprimir o resultado
+do b que é = 10
+
+no segundo ele vai imprimir a nova ordem que é a = 10 e b = 5
+*/
+
+/*
+a = 10
+b = 20
+c = a
+b = c
+a = b 
+
+console.log (a , b , c)
+*/
+
+/* 
+ele irá imprimir:
+a = 10
+b = 10
+c = 10
+*/
+
+/* Exercício 1 */
+
+/*
+let nome = ("Qual seu nome?")
+console.log (typeof nome)
+
+
+let idade = ("Qual sua idade?")
+console.log (typeof idade)
+*/
+
+/* d) Reflita: por que esse tipo foi impresso? Escreva a 
+resposta em um comentário de código.
+Resposta: string*/
+
+/*
+let nome = prompt ("Qual seu nome?")
+// console.log(nome)
+
+let idade = prompt ("Qual sua idade?")
+// console.log(idade)
+*/
+
+/*e) Novamente, imprima na tela o tipo dessas variáveis. 
+O que você notou? Escreva em um comentário de código.
+Resposta: string*/
+
+/*
+console.log (`Olá ${nome}, você tem ${idade} anos`)
+*/
+
+/*
+let animal
+let comida
+let cor
+let cantora
+let musica
+
+animal = prompt ("Qual seu animal preferido?")
+comida = prompt ("Qual sua comida preferida?")
+cor = prompt ("Qual seu cor preferida?")
+cantora = prompt ("Qual sua cantora preferida?")
+musica = prompt ("Qual sua musica preferida?")
+
+console.log(`1. Qual seu animal favorito?  Resposta: ${animal}`)
+console.log(`2. Qual sua comida favorita?  Resposta: ${comida}`)
+console.log(`3. Qual sua cor favorita?  Resposta: ${cor}`)
+console.log(`4. Qual sua cantora favorita?  Resposta: ${cantora}`)
+console.log(`5. Qual sua musica favorita?  Resposta: ${musica}`)
+*/
+
+/*
+let foodTopFive = ["pizza", "sushi", "hamburguer", "maionese", "churrasco"]
+console.log(foodTopFive)
+console.log(`Essas são as minhas comidas preferidas: 
+${foodTopFive[0]}
+${foodTopFive[1]}
+${foodTopFive[2]}
+${foodTopFive[3]}
+${foodTopFive[4]}`)
+*/
+
+let questions = ["Você é brasileiro", "Você gosta de cachorro", "Você gosta de alho"]
+let answer = ["true", "false"]
+console.log(questions[0], answer[0])
+console.log(questions[1], answer[0])
+console.log(questions[2], answer[1])
