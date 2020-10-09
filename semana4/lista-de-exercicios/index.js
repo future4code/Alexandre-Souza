20245c46a8fb6d065c52f07757eacab547830e56
Alexandre <alexandre.yukon@gmail.com>
2020-10-09 17:58:48 -0300

    exercios leitura de codigo completo e finalizado

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,43 +1,102 @@
+//Exercícios de Interpretação de códigos
+
 //Exercício 1
 //Fará a multiplicação com o valor da cotação que o usuário inserir no prompt. Irá mostrar o valor da multiplicação.
 
 //Exercício 2
-//È uma função witch case: ações ocup valor de tipo de investimento que tem varios tipos, ações se torna valor. 
-//O segundo montante tesouro direto e não é igual a nada: entao vai pro alert, ou seja, undefinid pq o retur esta fora da função
+//O programa irá calcular o investimento que recebe parâmetros de tipo de investimento e de valor de investimento.
+//A tratativa será rodada em Switch case.
+//Irá rodar o parâmetro que se for diferente das opções marcadas, irá emitir um alert.
+//Se for igual ele irá gerar um montante que é o valor do parâmetro multilicado pelo valor de cada opção do switch case.
+//Irá guardar o valor do montante em uma variável e retornar como resultado processado pela função.
+//Irá imprimir o retorn de toda função no console navegador.
 
 //Exercício 3
-//Ele vai percorrer desde o i=0 até o último e verificar a paridade de todos, se for
+//Ele vai percorrer desde o i=0 até o último e verificar a paridade de todos, se for par
 //vai acrescentar esses números dentro do array1, se não for, vai para o array2.
 //depois irá imprimir a quantidade de cada instrução de console.log respectivamente: 14, pares e não pares.
 
 //Exercício 4
-//se 25 é menor que infinity, n1 se tornará 25
-//console 1: n1 vai se tornar o menor n possível, no caso o -10
-//console 2: maior número
+//Programa vai verificar a quantidade máxima e mínima de elementos dentro de números.
+//Se trata de um loop para verificação desses números.
+//Na primeira armazenará números menores que o anterior (1)
+//Na segunda armazenará números maiores (25)
+
+
+
+//Exercícios de lógica de Programação
+
+//Exercício 1
+
+//1 - for
+//2 - for of
+//3 - while
+
+// function bananinha(listaArray) {
+//     for(let i = 0; i < listaArray.lenght; i++) {
+//         console.log(listaArray[i])
+//     }
+    
+// }
+
+
+//Exercício 2
+
+// Boolean1 = true
+// Boolean2 = false
+// Boolean3 = true
+// Boolean4 = false
+
+// a) true && false && true = false
+// b) true && false || true = true
+// c) false || true && false || true = true
+// d) true && true || false && true = true
+// e) false && true || true && true && true = true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Exercício 5
 
 
-const numero1 = Number(prompt('Digite um número: '))
-const numero2 = Number(prompt('Digite outro número: '))
+// const numero1 = Number(prompt('Digite um número: '))
+// const numero2 = Number(prompt('Digite outro número: '))
 
 
-const verificaMaiorNumero = (a, b) => {
-    if ( a > b ) {
-         console.log('O maior é: ' , a)
-         console.log(b + ' não é divisível por ' + a)
-         console.log(a + ' é divisível por ' + b)
-         console.log('A diferença entre eles é',  a - b)
-    }else if ( b > a ) {
-         console.log('O maior é: ', b)
-         console.log(a + ' não é divisível por ' + b)
-         console.log(b + ' é divisível por' + a)
-         console.log('A diferença entre eles é ' , b - a)
-    } else {
-        alert('Os dois números são iguais!')
-    }
+// const verificaMaiorNumero = (a, b) => {
+//     if ( a > b ) {
+//          console.log('O maior é: ' , a)
+//          console.log(b + ' não é divisível por ' + a)
+//          console.log(a + ' é divisível por ' + b)
+//          console.log('A diferença entre eles é',  a - b)
+//     }else if ( b > a ) {
+//          console.log('O maior é: ', b)
+//          console.log(a + ' não é divisível por ' + b)
+//          console.log(b + ' é divisível por' + a)
+//          console.log('A diferença entre eles é ' , b - a)
+//     } else {
+//         alert('Os dois números são iguais!')
+//     }
 
-}
+// }
 
-verificaMaiorNumero(numero1, numero2)
+// verificaMaiorNumero(numero1, numero2)
