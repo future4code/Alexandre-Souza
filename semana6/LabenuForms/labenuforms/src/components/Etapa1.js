03690d7c5fac9cefe9ffa9852109373a12cbdbaa
Alexandre <alexandre.yukon@gmail.com>
2020-10-19 17:43:23 -0300

    labeforms

---
+++ semana6/LabenuForms/labenuforms/src/components/Etapa1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export const Etapa1 = () => {
+	return ( <div className="etapa-one">
+		<h2>etapa 1 - dados gerais</h2>
+		</div>
+		);
+}
+
+export default App;
