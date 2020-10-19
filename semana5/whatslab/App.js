adcd772d70e95faef5536a4f0bc6e783d04cff17
Alexandre <alexandre.yukon@gmail.com>
2020-10-19 15:44:46 -0300

    whatslab

---
+++ semana5/whatslab/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() extends React.Component {
+    render() {
+        return (
+        <div>
+            Whatslab
+        </div>
+        );
+        }
+}
+
+export default App;
