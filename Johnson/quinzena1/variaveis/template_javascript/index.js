5ab6e705475c9ebd938a2337d3c0b704bca61abf
Alexandre <alexandre.yukon@gmail.com>
2021-05-20 10:14:32 -0300

    Exercícios 1, 2 e 3 Aula de Variáveis

---
+++ Johnson/quinzena1/variaveis/template_javascript/index.js
@@ -0,0 +1,24 @@
+// Exercícios de interpretação de código
+
+// 1.
+// a = 10
+// b = 5
+
+// 2.
+// a = 10
+// b = 10
+// c = 10
+
+// 3.
+// let cargaHoraria = prompt("Quantas horas você trabalha por dia?")
+// let pagamentoDia = prompt("Quanto você recebe por dia?")
+// alert(`Você recebe ${pagamentoDia/cargaHoraria}`)
+
+
+//Exercícios de escrita de código
+
+let nome = prompt("Qual o seu nome?") 
+let idade = Number(prompt("Qual a sua idade?"))
+
+console.log(typeof nome)
+console.log(typeof idade)
