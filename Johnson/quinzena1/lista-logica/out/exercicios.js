2a4a1f094a7ed15b01462b3074d18ae548736ea0
Alexandre <alexandre.yukon@gmail.com>
2021-05-23 22:25:52 -0300

    Exercícios 1, 2 e 3 Projeto Quinzena1

---
+++ Johnson/quinzena1/lista-logica/out/exercicios.js
@@ -0,0 +1,94 @@
+// Exemplos
+
+// Exercício 0A
+function soma() {
+  // escreva seu código aqui
+  const num1 = prompt('Digite o primeiro número')
+  const num2 = prompt('Digite o segundo número')
+
+  console.log(Number(num1) + Number(num2))
+}
+
+// Exercício 0B
+function imprimeMensagem() {
+  // escreva seu código aqui
+  const mensagem = prompt('Digite sua mensagem')
+
+  console.log(mensagem)
+}
+
+// ---------------------------------------------------
+// Exercícios
+
+// Exercício 1
+function calculaAreaRetangulo() {
+  const altura = Number(prompt("Digite a altura do retângulo"));
+  const largura = Number(prompt("Digite a largura do retângulo"));
+  const resultado = (altura * largura);
+
+console.log(resultado);
+}
+
+// Exercício 2
+function imprimeIdade() {
+  const anoAtual = Number(prompt("Digite o ano atual"));
+  const anoSeuNascimento = Number(prompt("Digite o ano do seu nascimento"));
+  const resultado = (anoAtual - anoSeuNascimento);
+
+console.log(resultado);
+}
+
+// Exercício 3
+function calculaIMC() {
+  const seuPeso = Number(prompt("Quantos quilos você pesa?"));
+  const suaAltura = Number(prompt("Qual a sua altura?"));
+  const resultadoIMC = seuPeso / (suaAltura * suaAltura)
+
+console.log(resultadoIMC);
+}
+
+
+// Exercício 4
+function imprimeInformacoesUsuario() {
+  // escreva seu código aqui
+}
+
+// Exercício 5
+function imprimeTresCoresFavoritas() {
+  // escreva seu código aqui
+}
+
+// Exercício 6
+function retornaStringEmMaiuscula() {
+  // escreva seu código aqui
+}
+
+// Exercício 7
+function calculaIngressosEspetaculo() {
+  // escreva seu código aqui
+}
+
+// Exercício 8
+function checaStringsMesmoTamanho() {
+  // escreva seu código aqui
+}
+
+// Exercício 9
+function checaIgualdadeDesconsiderandoCase() {
+  // escreva seu código aqui
+}
+
+// Exercício 10
+function checaRenovacaoRG() {
+  // escreva seu código aqui
+}
+
+// Exercício 11
+function checaAnoBissexto() {
+  // escreva seu código aqui
+}
+
+// Exercício 12
+function checaValidadeInscricaoLabenu() {
+  // escreva seu código aqui
+}
