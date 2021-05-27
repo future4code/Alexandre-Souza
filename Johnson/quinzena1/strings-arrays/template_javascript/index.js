250546720dcfcffccdebc651bc99440b0203e6e0
Alexandre <alexandre.yukon@gmail.com>
2021-05-27 20:15:42 -0300

    Exercício 2 de Escrita de código Strings e Arrays

--- Johnson/quinzena1/strings-arrays/template_javascript/index.js
+++ Johnson/quinzena1/strings-arrays/template_javascript/index.js
@@ -3,6 +3,18 @@
 // Exercícios de interpretação de código
 
 //1.
+//a. imprime o undefined
+//b. imprime array null
+//c. imprime tamanho do array: 11
+//d. 3 é o novo index
+//e. 3 se torna 19 e imprime: [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//f. valor 9
+
+//2.
+// SUBI NO ONIBUS EM MIRROCOS
+//frase.length: 26
+
+
 
 
 
