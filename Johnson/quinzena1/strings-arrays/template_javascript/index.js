39ee0da0351f1a386ed9cfd7cc0061633c546643
Alexandre <alexandre.yukon@gmail.com>
2021-05-27 23:43:23 -0300

    Envio exercícios finalizados Strings e Arrays

--- Johnson/quinzena1/strings-arrays/template_javascript/index.js
+++ Johnson/quinzena1/strings-arrays/template_javascript/index.js
@@ -14,10 +14,48 @@
 // SUBI NO ONIBUS EM MIRROCOS
 //frase.length: 26
 
+//Exercícios de escrita de código
+//1.
+// const nomeUsuario = prompt("Qual o seu nome?");
+// const emailUsuario = prompt("Qual o seu email?");
+
+// console.log(`O e-mail ${emailUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeUsuario}`);
+
+//2.
+//a.
+//const comidasPreferidas = ["sushi", "churrasco", "feijoada", "macarronada", "pizza"];
+// console.log("comidas", comidasPreferidas);
+
+//b.
+// console.log("Essas são minhas comidas preferidas:");
+// console.log("Comida número 1:",comidasPreferidas[0]);
+// console.log("Comida número 2:",comidasPreferidas[1]);
+// console.log("Comida número 3:",comidasPreferidas[2]);
+// console.log("Comida número 4:",comidasPreferidas[3]);
+// console.log("Comida número 5:",comidasPreferidas[4]);
+
+//c.
+// const comidaFavorita = prompt("Qual a sua comida preferida?");
+// const comidaUsuario = [comidaFavorita];
+// console.log(comidaUsuario);
+
+// console.log("Comida número 1:",comidasPreferidas[0]);
+// console.log("Comida número 2:",comidaUsuario);
+// console.log("Comida número 3:",comidasPreferidas[2]);
+// console.log("Comida número 4:",comidasPreferidas[3]);
+// console.log("Comida número 5:",comidasPreferidas[4]);
 
 
+//3.
+//a.
+let tarefas =[];
 
+const tarefa1 = prompt("Digite a primeira tarefa do seu dia");
+const tarefa2 = prompt("Digite a segunda tarefa do seu dia");
+const tarefa3 = prompt("Digite a terceira tarefa do seu dia");
 
+tarefas.push(tarefa1, tarefa2, tarefa3)
 
-
-
+const excluirUmaTarefa = prompt("Escolha: 0, 1 ou 2")
+tarefas.splice(excluirUmaTarefa, 1)
+console.log(tarefas)
