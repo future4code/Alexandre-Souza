cd84e5a872e72b121cf06d269fca8f7eaa5b4fbf
Alexandre <alexandre.yukon@gmail.com>
2021-06-01 10:56:20 -0300

    Exercício 1 de interpretação de código

---
+++ Johnson/quinzena1/funcoes/template_javascript/index.js
@@ -0,0 +1,11 @@
+//Exercícios de interpretação de código
+
+//1.
+//função irá retornar a multiplicação da variável 5 por 2 e por 10.
+
+//a. Será impresso 10 e 50 no console
+
+//b. Dá erro se tirar o console.log
+
+
+
