2a55bb107ae492da25c9cfd3c8e6d9bf8301a3e1
Alexandre <alexandre.yukon@gmail.com>
2021-06-03 14:09:27 -0300

    Exercício 2 de interpretação de código

--- Johnson/quinzena1/objetos/index.js
+++ Johnson/quinzena1/objetos/index.js
@@ -5,3 +5,17 @@
 //console.log(filme.elenco[filme.elenco.length - 1]) vai imprimir Virginia Cavendish
 //console.log(filme.transmissõesHoje[2]) vai imprimir o array 2 que é Globo", horario: "14h
 
+//2.
+//a.
+//console.log(cachorro)
+// nome: "Juca", idade: 3, raca: "SRD"
+
+//console.log(gato)
+//nome: "Juba", idade: 3, raca: "SRD"
+
+//console.log(tartaruga)
+//nome: "Jubo", idade: 3, raca: "SRD"
+
+//b.
+//Ele faz uma cópia
+
