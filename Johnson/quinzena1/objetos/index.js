27473a158e3b4cbbfa31a61f677b0b882a5fd95b
Alexandre <alexandre.yukon@gmail.com>
2021-06-03 13:45:09 -0300

    Exercício 1 de interpretação de código

---
+++ Johnson/quinzena1/objetos/index.js
@@ -0,0 +1,7 @@
+//Exercícios de interpretação de código
+//1.
+//a.
+//console.log(filme.elenco[0]) vai imprimir Matheus Nachtergaele
+//console.log(filme.elenco[filme.elenco.length - 1]) vai imprimir Virginia Cavendish
+//console.log(filme.transmissõesHoje[2]) vai imprimir o array 2 que é Globo", horario: "14h
+
