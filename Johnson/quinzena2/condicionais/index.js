9f4a2798fbd69e96b0d61a819434c9039c2a0060
Alexandre <alexandre.yukon@gmail.com>
2021-06-07 23:00:28 -0300

    Exercício 2 de interpretação de código

--- Johnson/quinzena2/condicionais/index.js
+++ Johnson/quinzena2/condicionais/index.js
@@ -6,5 +6,9 @@
 
 //c. Não passa no teste os números que restam diferente de zero.
 
+//2.
+//a. O código acima serve para o selecionar o preço de uma fruta que pode ter na lista, caso tenha será informado o valor, caso não tenha, será informado o valor de default que é 5.
 
+//b. Maçã será informado o valor de 2.25
 
+//c. Pêra e default serão considerados como 5
