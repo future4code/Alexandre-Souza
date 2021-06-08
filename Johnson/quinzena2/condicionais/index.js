f628c3925ff9ecac1d2431bd33f15f725e5b15ad
Alexandre <alexandre.yukon@gmail.com>
2021-06-07 22:50:49 -0300

    Exercício 1 de interpretação de código

---
+++ Johnson/quinzena2/condicionais/index.js
@@ -0,0 +1,10 @@
+//Exercícios de interpretação de código
+//1.
+//a. É um if else, dá uma condição, caso não for if, é else.
+
+//b. Passa no teste os números que resta zero.
+
+//c. Não passa no teste os números que restam diferente de zero.
+
+
+
