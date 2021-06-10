e444259ce762fa4a6ebcc25a8fe0dcc551c13b85
Alexandre <alexandre.yukon@gmail.com>
2021-06-10 16:36:47 -0300

    Exercícios 1 e 2 de interpretação de código

---
+++ Johnson/quinzena2/loops/index.js
@@ -0,0 +1,16 @@
+//Exercícios de interpretação de código
+//1. Esse laço roda 5 vezes! Mas ele só aumenta o valor de i no i++  da linha do for. Dentro dele, no código que faz valor += i  a gente adicionar o valor atual de i  na variável valor , sem mudar a variável i .
+// Então a ideia é que valor vai ter 0 no começo.
+// Na primeira iteração, i  é igual a 0, então valor  fica igual a 0 + 0, ou seja 0
+// Na segunda iteração, i  é igual a 1, então valor  fica igual a 0 + 1, ou seja 1
+// Na terceira iteração, i  é igual a 2. então valor  fica igual a 1 + 2, ou seja 3
+// Na quarta iteração, i  é igual a 3, então valor  fica igual a 3 + 3, ou seja 6
+// Na quinta iteração, i  é igual a 4, então valor  fica igual a 6 + 4, ou seja 10
+
+//2. 
+//a. Imprime todos os números maiores de 18
+
+//b. O for... of... não é suficiente pois ele não é capaz de acessar cada índice como o for normal.
+
+
+
