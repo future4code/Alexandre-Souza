e72c52b7aa0fef57aea8e031f8e0c498c23f9666
Alexandre <alexandre.yukon@gmail.com>
2021-06-14 22:09:54 -0300

    Exercício 1 de interpretação de código

---
+++ Johnson/quinzena2/funcoes-de-array/index.js
@@ -0,0 +1,4 @@
+// Exercícios de interpretação de código
+
+// 1.
+// É um map, que vai percorrer cada item, colocar o índice do array, mostrando que cada nome tem seu apelido e onde está no seu índice
